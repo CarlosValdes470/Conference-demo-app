b58c3a8833fb2f739883f6cd7cebbc043ec7e22f
Carlos Valdes <carlosvaldes8470@gmail.com>
2023-04-23 13:30:21 -0400

    initial commit

---
+++ src/main/java/com/pluralsight/conferencedemo/config/persistenceConfiguration.java
@@ -0,0 +1,22 @@
+package com.pluralsight.conferencedemo.config;
+
+//import javax.sql.DataSource;
+
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class persistenceConfiguration {
+    
+  /*  @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder<?> builder = DataSourceBuilder.create();
+        builder.url("");
+        builder.username("");
+        builder.password("");
+        System.out.println("My custom datasource bean has been iitialized and set");
+        return builder.build();
+    }
+    */
+}
---
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -0,0 +1,55 @@
+package com.pluralsight.conferencedemo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pluralsight.conferencedemo.models.Speaker;
+import com.pluralsight.conferencedemo.repositories.SpeakerRepository;
+
+@RestController
+@RequestMapping("/api/v1/speakers")
+public class SpeakersController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list(){
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id){
+        return speakerRepository.getReferenceById(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        //Also need to check for children records prior to deleting.
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        //because this is a PUT, we expect all attributes to be passed in. A PATCH would only need what
+        //TODO: Add validation that all attributes are passed in, otherwise return 400 bad payload
+
+        Speaker existingSpeaker = speakerRepository.getReferenceById(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
